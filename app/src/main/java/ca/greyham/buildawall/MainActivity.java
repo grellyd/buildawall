1a84bb268dd70d957d1a65f4112bae9adbabe2c6
Tanya <tanyi3025@gmail.com>
2016-02-27 22:21:05 -0800

    finish mainActivity: fire to another acitivy

--- app/src/main/java/ca/greyham/buildawall/MainActivity.java
+++ app/src/main/java/ca/greyham/buildawall/MainActivity.java
@@ -1,17 +1,26 @@
 package ca.greyham.buildawall;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     //TODO: Fire intent to launch selectionActivity
-    //TODO: Build and populate the lancher MemberOC list
+    //TODO: Build and populate the launcher MemberOC list
     //TODO: Query the server for bill tracker details
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //new
+    public void buttonGo_Click(View v)
+    {
+        Intent selectionActivity = new Intent(MainActivity.this, SelectionActivity.class);
+        startActivity(selectionActivity);
     }
 }
