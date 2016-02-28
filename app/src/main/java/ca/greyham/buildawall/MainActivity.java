bfb77f2738d78674554ac7a43d205b1bc8a6ea53
Lellow <graham.brown137@gmail.com>
2016-02-27 22:12:51 -0800

    Added Yes/No Fragment

--- app/src/main/java/ca/greyham/buildawall/MainActivity.java
+++ app/src/main/java/ca/greyham/buildawall/MainActivity.java
@@ -1,17 +1,40 @@
 package ca.greyham.buildawall;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class MainActivity extends AppCompatActivity {
 
     //TODO: Fire intent to launch selectionActivity
-    //TODO: Build and populate the lancher MemberOC list
+    //TODO: Build and populate the launcher MemberOC list
     //TODO: Query the server for bill tracker details
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // TODO: Get server world state
+        // TBRemoved
+        List<MemberOC> mocList = new ArrayList<MemberOC>();
+
+
+        for (MemberOC moc : mocList) {
+            fragmentManager.beginTransaction();
+            Fragment newMOCFragment = MOCFragment.newInstance("", "");
+            fragmentTransaction.add(R.id.mainActivityScrollView, newMOCFragment,"");
+        }
     }
 }
