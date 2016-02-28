32b99dc7437e7115830240da1608756841d5cefa
Lellow <graham.brown137@gmail.com>
2016-02-27 20:36:41 -0800

    More TODO notes.

--- app/src/main/java/ca/greyham/buildawall/SelectionActivity.java
+++ app/src/main/java/ca/greyham/buildawall/SelectionActivity.java
@@ -6,8 +6,12 @@ import android.os.Bundle;
 public class SelectionActivity extends AppCompatActivity {
 
     //TODO: Query the server for the client ID of the MemberOC who is selecting
-    //TODO: For that MOC, show the form committee fragment
-    //TODO: 
+    //TODO: For that MOC, show the form committee fragment, and blank for the others
+    //TODO: After the selection is made, send the results to the server with the chosen members
+    //TODO: For other MOCs show the committee approval fragment, and blank for the leader
+    //TODO: Show if the selection passed. If not, return to step one with the query
+    //TODO: If passed then fire the intent to the vote activity
+    //TODO: If failed five times, fire the intent to the main_activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
